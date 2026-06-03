8e70ff33e5dd16cb833ad7f68f0be82ae05f6383
Halusitz Dániel <daniel.halusitz@gmail.com>
2026-06-03 16:42:38 +0200

    #1 add google access token update

--- src/auth/services/auth-google.service.ts
+++ src/auth/services/auth-google.service.ts
@@ -1,12 +1,17 @@
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { Credentials } from 'google-auth-library';
+import { Credentials, OAuth2Client } from 'google-auth-library';
+import { Model } from 'mongoose';
 import { GoogleClientService } from 'src/google-client/google-client.service';
+import { encrypt } from 'src/utils/encrypt';
 
 import {
   AuthGoogleLoginDTO,
   AuthGoogleLoginResponseDTO,
 } from '../dto/auth-google-logn.dto';
+import { ExternalAccountType } from '../enum/external-account-type.enum';
+import { ExternalAccount } from '../schemas/external-account.schema';
 
 type GoogleUser = {
   id: string;
@@ -19,12 +24,71 @@ type GoogleUser = {
 export class AuthGoogleService {
   private readonly logger: Logger = new Logger('AuthGoogleService');
 
-  constructor(private readonly googleClientService: GoogleClientService) {}
+  constructor(
+    @InjectModel(ExternalAccount.name)
+    private readonly externalAccountModel: Model<ExternalAccount>,
 
-  /*   @Cron(CronExpression.EVERY_10_MINUTES)
-  private async updateTokens() {
+    private readonly googleClientService: GoogleClientService,
+  ) {}
 
-  } */
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  private async updateAccessTokens() {
+    this.logger.log('Start update access tokens');
+    const cursor = this.externalAccountModel
+      .find({
+        type: ExternalAccountType.GOOGLE,
+        expiryDate: {
+          $lte: Date.now() + 10 * 60 * 1000,
+        },
+      })
+      .lean()
+      .cursor();
+
+    let promises: Promise<void>[] = [];
+    for await (const googleAccount of cursor) {
+      promises.push(this.updateAccessToken(googleAccount));
+      if (promises.length === 100) {
+        await Promise.allSettled(promises);
+        promises = [];
+      }
+    }
+
+    if (promises.length > 0) {
+      await Promise.allSettled(promises);
+    }
+
+    this.logger.log('End update access tokens');
+  }
+
+  private async updateAccessToken(googleAccount: ExternalAccount) {
+    const newCredentials = await this.googleClientService.run(
+      googleAccount,
+      async (client: OAuth2Client) => {
+        const { credentials } = await client.refreshAccessToken();
+
+        return credentials;
+      },
+    );
+
+    const { access_token, expiry_date, refresh_token } = newCredentials;
+    if (!access_token || expiry_date === null || expiry_date === undefined) {
+      this.logger.error(`Missing tokens at user: ${googleAccount.userId}`);
+      return;
+    }
+
+    await this.externalAccountModel.updateOne(
+      {
+        _id: googleAccount._id,
+      },
+      {
+        accessTokenEncrypted: encrypt(access_token),
+        ...(refresh_token
+          ? { refreshTokenEncrypted: encrypt(refresh_token) }
+          : {}),
+        expiryDate: expiry_date,
+      },
+    );
+  }
 
   async login(
     payload: AuthGoogleLoginDTO,
